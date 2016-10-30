b387f68a323bcb42674e3d51acb3e9bc83118e67
Matthew Swan <swanm86@gmail.com>
2016-10-30 10:35:39 -0500

    get the latest version of master code

---
+++ CleanSweep/test/cleansweep/CleanSweepImplTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cleansweep;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author MatthewSwan
+ */
+public class CleanSweepImplTest {
+    CleanSweep cleanSweep;
+    
+    public CleanSweepImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+}
